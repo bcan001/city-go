0187eb2b82a419b5c7cb1ec25c9d7e7e5c62e902
bcaneba <bcaneba@gmail.com>
2018-05-29 23:13:51 -0500

    added event show

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,13 +9,11 @@ class EventsController < ApplicationController
 		@featured_events = Feature.featured_events(5)
 		@featured_venues = Feature.featured_venues(5)
 
-
 	end
 
 	def show
-		@event = Event.find(params[:id])
+		@event = Event.joins(:artist,:venue).find(params[:id])
 	end
-
 
 
 
