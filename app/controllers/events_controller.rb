30ab62583e69b6f0a15a0b44ad6e93326d74159a
bcaneba <bcaneba@gmail.com>
2018-05-31 23:25:50 -0500

    added tomorrows events page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,6 @@
 class EventsController < ApplicationController
 
 	def index
-
 		# RSpotify.authenticate("", "")
 		# artists = RSpotify::Artist.search('Arctic Monkeys')
 
@@ -17,8 +16,17 @@ class EventsController < ApplicationController
 	end
 
 
-
-
-
+	def tomorrows_events
+		@events = Event.tomorrows_events(10)
+		@featured_events = Feature.featured_events(5)
+		@featured_venues = Feature.featured_venues(5)
+	end
 
 end
+
+
+
+
+
+
+
