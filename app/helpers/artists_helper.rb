f5320f82ad159355264694ac783b091f394f3e32
bcaneba <bcaneba@gmail.com>
2018-07-02 22:50:09 -0500

    added in spotify images for artists

---
+++ app/helpers/artists_helper.rb
@@ -0,0 +1,37 @@
+module ArtistsHelper
+
+	def fetch_spotify_image(artist_name,type)
+		begin
+			RSpotify.authenticate(ENV["CLIENT_ID"],ENV["CLIENT_SECRET"])
+			artist = RSpotify::Artist.search(artist_name)
+			return artist[0].images[0]['url']
+		rescue
+			if type == 'event'
+				return '/assets/event_banner.png'
+			elsif type == 'artist'
+				return '/assets/artist_profile.png'
+			end
+		end
+	end
+
+	 # [#<RSpotify::Artist:0x00007ff530ca5ee0
+ #  @external_urls={"spotify"=>"https://open.spotify.com/artist/0Y0QSi6lz1bPik5Ffjr8sd"},
+ #  @followers={"href"=>nil, "total"=>37916},
+ #  @genres=["bass trap", "brostep", "edm", "electro house", "electronic trap", "moombahton", "vapor twitch"],
+ #  @href="https://api.spotify.com/v1/artists/0Y0QSi6lz1bPik5Ffjr8sd",
+ #  @id="0Y0QSi6lz1bPik5Ffjr8sd",
+ #  @images=
+ #   [{"height"=>640, "url"=>"https://i.scdn.co/image/1aab1fe3ced941849a9cece2acbb28472eab23a8", "width"=>640},
+ #    {"height"=>320, "url"=>"https://i.scdn.co/image/94de60e93b3f4e47abbd0fe6f6c90ba8f572bd22", "width"=>320},
+ #    {"height"=>160, "url"=>"https://i.scdn.co/image/a2c31ba62db794a2bfba869413c4b9b7f1d9bc3f", "width"=>160}],
+ #  @name="Ekali",
+ #  @popularity=55,
+ #  @top_tracks={},
+ #  @type="artist",
+ #  @uri="spotify:artist:0Y0QSi6lz1bPik5Ffjr8sd">]
+
+
+
+
+
+end
