4f221a9ff66bcc785bf5a36df46dffff62e34ee6
bcaneba <bcaneba@gmail.com>
2018-07-14 10:02:26 -0500

    fixed advertisement images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,22 @@ module ApplicationHelper
 		date.strftime("%A %B %d, %I:%M %p")
 	end
 
+
+	# def advertisement_image(attrs)
+	# 	if attrs[:size] == 'long'
+
+	# 	elsif attrs[:size] == 'small'
+
+	# 	elsif attrs[:size] == 'tall'
+
+	# 	elsif attrs[:size] == 'tall2'
+
+	# 	elsif attrs[:size] == 'tall3'
+
+	# 	elsif attrs[:size] == 'long'
+
+	# 	end
+	# end
+
+
 end
